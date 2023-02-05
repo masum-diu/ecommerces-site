d9e8fa446bc3125430259e9d70c8a407d497af17
“masum182” <masum.mollah@webable.digital>
2023-02-05 17:03:28 +0600

    sign up

--- components/SignInModal.js
+++ components/SignInModal.js
@@ -7,12 +7,36 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { IconButton, Stack, TextField } from "@mui/material";
 import { MdClose } from "react-icons/md";
-
+import { useForm } from "react-hook-form";
+import axios from "axios"
 const SignInModal = ({ open, setOpen,signModal }) => {
   const handleChange = () => {
     setOpen(false);
     signModal(true);
   };
+  
+   const { register, handleSubmit } = useForm({
+    defaultValues: {
+      name: '',
+      email:'',
+      password:'',
+      confirm_password:''
+      
+    }
+   
+  });
+
+  const onSubmit=(data)=>{
+    axios.post('http://apiaranya.jumriz.com/public/api/auth/register',data,{
+     
+  })
+  .then(result=>{
+    console.log(result.data)
+    localStorage.setItem('acesstoken',result.data.token)
+    
+  }).catch(err=>{
+    console.log(err)
+  })}
   return (
     <>
       <Dialog
@@ -39,18 +63,34 @@ const SignInModal = ({ open, setOpen,signModal }) => {
         </DialogTitle>
         <DialogContent>
           <DialogContentText>
+          <form  onSubmit={handleSubmit(onSubmit)}>
             <Stack direction={"column"} spacing={3} mt={2} mb={2}>
               <Stack direction={"column"} spacing={1}>
                 <Typography variant="cardHeader12" color="initial">
-                EMAIL ADDRESS 
+                USERNAME
                 </Typography>
                 <TextField
+                 {...register("name", { required: true })}
                   id=""
                   label=""
                   // value={}
                   // onChange={}
                   size="small"
-                  placeholder="Username or Email Address* "
+                  placeholder="Username* "
+                />
+              </Stack>
+              <Stack direction={"column"} spacing={1}>
+                <Typography variant="cardHeader12" color="initial">
+                EMAIL ADDRESS 
+                </Typography>
+                <TextField
+                 {...register("email", { required: true })}
+                  id=""
+                  label=""
+                  // value={}
+                  // onChange={}
+                  size="small"
+                  placeholder="Email Address* "
                 />
               </Stack>
               <Stack direction={"column"} spacing={1}>
@@ -58,6 +98,21 @@ const SignInModal = ({ open, setOpen,signModal }) => {
                   PASSWORD
                 </Typography>
                 <TextField
+                 {...register("password", { required: true })}
+                  id=""
+                  label=""
+                  // value={}
+                  // onChange={}
+                  size="small"
+                  placeholder="Password*"
+                />
+              </Stack>
+              <Stack direction={"column"} spacing={1}>
+                <Typography variant="cardHeader12" color="initial">
+                 CONFIRM PASSWORD
+                </Typography>
+                <TextField
+                 {...register("confirm_password", { required: true })}
                   id=""
                   label=""
                   // value={}
@@ -68,13 +123,14 @@ const SignInModal = ({ open, setOpen,signModal }) => {
               </Stack>
 
               
-              <Button variant="contained" color="background2">
+              <Button variant="contained" color="background2" type="submit">
                 Register
               </Button>
               <Typography variant="cardHeader12" textAlign={"center"} sx={{cursor:"pointer"}} color="initial" onClick={handleChange}>
                 New to Aranya?  <span style={{color:"#75879D"}}>  Already have an Account</span> 
               </Typography>
             </Stack>
+            </form>
           </DialogContentText>
         </DialogContent>
       </Dialog>
