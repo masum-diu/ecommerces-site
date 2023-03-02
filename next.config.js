82358d533a43ce1aa9f9c11680ab890784e79318
shamimul.haque <shamimul.haque@webable.digital>
2023-03-02 11:14:02 +0600

    some changes done

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
+const webpack = require("webpack");
 const nextConfig = {
-  reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery",
+      })
+    );
+    return config;
+  },
+
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "api.lorem.space",
+        hostname: "cloudinary.com",
       },
     ],
   },
