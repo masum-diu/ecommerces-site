81e2a606bfe6eb8c3df0b715abcdf42a661c3187
shamimul.haque <shamimul.haque@webable.digital>
2025-03-25 15:58:02 +0600

    gtm is updated

--- pages/_app.js
+++ pages/_app.js
@@ -45,33 +45,51 @@ export default function MyApp(props) {
           `,
         }}
       />
-      <script
+      {/* <Script
         dangerouslySetInnerHTML={{
           __html: `window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', 'UA-105579019-2')`,
+        gtag('config', 'UA-105579019-2', {
+              page_path: window.location.pathname,
+              })`,
         }}
       />
-      <script
+      <Script
         async
+        strategy="lazyOnload"
         src="https://www.googletagmanager.com/gtag/js?id=UA-105579019-2"
+      /> */}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-59BYS90FJD`}
       />
+
+      <Script id="" strategy="lazyOnload">
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-59BYS90FJD', {
+              page_path: window.location.pathname,
+              });
+          `}
+      </Script>
       {/* Google tag manager */}
       <Script
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-                      (function(w,d,s,l,i){
-                        w[l]=w[l]||[];
-                        w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});
-                        var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                        j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                        f.parentNode.insertBefore(j,f);
-                      })(window,document,'script','dataLayer','GTM-PZG4MP8');
-                    `,
+                    (function(w,d,s,l,i){
+                      w[l]=w[l]||[];
+                      w[l].push({'gtm.start':
+                      new Date().getTime(),event:'gtm.js'});
+                      var f=d.getElementsByTagName(s)[0],
+                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                      j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                      f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-PZG4MP8');
+                  `,
         }}
       />
       {/* <Pixel name="FACEBOOK_PIXEL_1" /> */}
